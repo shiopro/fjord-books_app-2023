01f840735de69c0713e88e66c5225cbca0a760e9
nagano <shionaga0214@gmail.com>
2025-04-29 15:12:28 +0900

    ユーザーの一覧ページ作成

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @users = User.order(:id).page(params[:page]).per(4)
+  end
+
+  def show
+    @user = User.find(params[:id])
+  end
+end
