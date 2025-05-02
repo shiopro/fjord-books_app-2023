37bd4f9e3992740c9c40914dc32a1bd28ee1f52a
nagano <shionaga0214@gmail.com>
2025-05-02 13:42:01 +0900

    rubocopチェック　の修正

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,6 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:account_update, keys: [:postal_code, :address, :introduction])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[postal_code address introduction])
   end
 end
