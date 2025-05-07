5f5cdbc948ad9112e43c07251ee893e010370c7b
nagano <shionaga0214@gmail.com>
2025-05-07 15:47:36 +0900

    アップロード可能なファイルをjpg, png, gifの3種類だけに限定した

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  validate :verify_file_type
   has_one_attached :icon
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  private
+
+  def verify_file_type
+    return unless icon.attached?
+
+    allowed_file_types = %w[image/jpeg image/png image/gif]
+    errors.add(:icon, :invalid_file_type) unless allowed_file_types.include?(icon.blob.content_type)
+  end
 end
