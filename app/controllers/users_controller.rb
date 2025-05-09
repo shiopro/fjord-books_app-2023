6a9389eb01da28de2f4a0be2dd667fd25f57f83c
nagano <shionaga0214@gmail.com>
2025-05-09 15:56:42 +0900

    ApplicationControllerに共通化済みのため冗長なコードを削除

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+
   def index
     @users = User.order(:id).page(params[:page])
   end
