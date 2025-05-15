9bb0250a2402c2f79a74aa31b7b7abb1d631387f
nagano <shionaga0214@gmail.com>
2025-05-15 23:56:50 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,11 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
   root to: 'books#index'
-  resources :books
+  resources :books do
+    resources :comments, only: :create, module: :books
+  end
   resources :users, only: %i(index show)
-  resources :reports
+  resources :reports do
+    resources :comments, only: :create, module: :reports
+  end
 end
