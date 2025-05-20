25939bd8de1a50e8bf4e0c58ac768cb86f7dd54f
nagano <shionaga0214@gmail.com>
2025-05-20 23:20:52 +0900

    コメント削除・編集できるようにした

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_comment, only: %i[edit update destroy]
+
+  def edit; end
+
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
@@ -12,7 +16,25 @@ class CommentsController < ApplicationController
     end
   end
 
+  def update
+    if @comment.update(comment_params)
+      redirect_to @commentable, notice: 'Comment was successfully updated.'
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @comment.destroy
+
+    redirect_to @commentable, notice: 'Comment was successfully destroyed.'
+  end
+
   private
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
 
   def comment_params
     params.require(:comment).permit(:content)
--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'books#index'
   resources :books do
-    resources :comments, only: :create, module: :books
+    resources :comments, only: %i[create edit update destroy], module: :books
   end
   resources :users, only: %i(index show)
   resources :reports do
-    resources :comments, only: :create, module: :reports
+    resources :comments, only: %i[create edit update destroy], module: :reports
   end
 end
