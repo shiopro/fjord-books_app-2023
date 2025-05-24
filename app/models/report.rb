232497a77511bf3eed6284ea9a2adadd513bac5e
nagano <shionaga0214@gmail.com>
2025-05-24 15:59:52 +0900

    多対多の関係性を作る

--- app/models/report.rb
+++ app/models/report.rb
@@ -4,6 +4,12 @@ class Report < ApplicationRecord
   belongs_to :user
   has_many :comments, as: :commentable, dependent: :destroy
 
+  has_many :report_mentions, class_name: 'ReportMention', foreign_key: 'source_report_id', inverse_of: :source_report, dependent: :destroy
+  has_many :mentioned_reports, through: :report_mentions, source: :target_report
+
+  has_many :mentions_received, class_name: 'ReportMention', foreign_key: 'target_report_id', inverse_of: :target_report, dependent: :destroy
+  has_many :referencing_reports, through: :mentions_received, source: :source_report
+
   validates :title, presence: true
   validates :content, presence: true
 
