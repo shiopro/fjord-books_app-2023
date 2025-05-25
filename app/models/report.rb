2ea66e5d6e6cde7baf4026c140ba25b3d8651ec0
nagano <shionaga0214@gmail.com>
2025-05-25 19:12:17 +0900

    言及関係を保存できるようにした

--- app/models/report.rb
+++ app/models/report.rb
@@ -13,11 +13,20 @@ class Report < ApplicationRecord
   validates :title, presence: true
   validates :content, presence: true
 
+  after_save :save_mentions
+
   def editable?(target_user)
     user == target_user
   end
 
   def created_on
     created_at.to_date
+  end
+
+  def save_mentions
+    report_mentions.destroy_all
+    ids = content.to_s.scan(%r{http://localhost:3000/reports/(\d+)}).flatten.uniq
+    reports = Report.where(id: ids).where.not(id:)
+    self.mentioned_reports = reports
   end
 end
