2939e61102d1c8ded2d1c430bb991f52502074fd
nagano <shionaga0214@gmail.com>
2025-06-01 00:12:17 +0900

    jsonは利用していないので不要なコメントアウトを削除

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,12 +4,12 @@ class ReportsController < ApplicationController
   before_action :set_report, only: %i[show edit update destroy]
   before_action :authorize_user!, only: %i[edit update destroy]
 
-  # GET /reports or /reports.json
+  # GET /reports
   def index
     @reports = Report.all
   end
 
-  # GET /reports/1 or /reports/1.json
+  # GET /reports/1
   def show
     @comment = Comment.new
     @comments = @report.comments
@@ -23,7 +23,7 @@ class ReportsController < ApplicationController
   # GET /reports/1/edit
   def edit; end
 
-  # POST /reports or /reports.json
+  # POST /reports
   def create
     @report = current_user.reports.build(report_params)
 
@@ -34,7 +34,7 @@ class ReportsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /reports/1 or /reports/1.json
+  # PATCH/PUT /reports/1
   def update
     if @report.update(report_params)
       redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
@@ -43,7 +43,7 @@ class ReportsController < ApplicationController
     end
   end
 
-  # DELETE /reports/1 or /reports/1.json
+  # DELETE /reports/1
   def destroy
     @report.destroy
 
