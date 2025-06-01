6f8dc6b7fede91cc7a4847956aab025449c00ff2
nagano <shionaga0214@gmail.com>
2025-06-02 00:34:09 +0900

    権限操作をbefore_actionでまとめた

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,7 @@
 
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit update destroy]
+  before_action :authorize_user!, only: %i[edit update destroy]
 
   def edit; end
 
@@ -17,8 +18,6 @@ class CommentsController < ApplicationController
   end
 
   def update
-    return redirect_to root_path, alert: '権限がありません' unless @comment.user == current_user
-
     if @comment.update(comment_params)
       redirect_to @commentable, notice: t('controllers.common.notice_update', name: Comment.model_name.human)
     else
@@ -27,13 +26,15 @@ class CommentsController < ApplicationController
   end
 
   def destroy
-    return redirect_to root_path, alert: '権限がありません' unless @comment.user == current_user
-
     @comment.destroy
     redirect_to @commentable, notice: t('controllers.common.notice_destroy', name: Comment.model_name.human)
   end
 
   private
+
+  def authorize_user!
+    redirect_to root_path, alert: '権限がありません' unless @comment.user == current_user
+  end
 
   def set_comment
     @comment = Comment.find(params[:id])
