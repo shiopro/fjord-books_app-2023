2f3445931a080442cc3439ae2c123d8aa1449951
nagano <shionaga0214@gmail.com>
2025-06-09 22:32:19 +0900

    日報の更新とともにメンションも更新されるように修正

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -33,11 +33,17 @@ class ReportsController < ApplicationController
   end
 
   def update
-    if @report.update(report_params)
-      redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
-    else
-      render :edit, status: :unprocessable_entity
+    @report.assign_attributes(report_params)
+
+    Report.transaction do
+      @report.save!
+      @report.save_mentions!
     end
+
+    redirect_to @report, notice: t('controllers.common.notice_update', name: Report.model_name.human)
+  rescue StandardError => e
+    flash.now[:alert] = "更新に失敗しました: #{e.message}"
+    render :edit, status: :unprocessable_entity
   end
 
   def destroy
