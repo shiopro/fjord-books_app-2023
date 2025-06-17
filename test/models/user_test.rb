e79cede0c8aec93dc520541d1fbdec024b06038d
nagano <shionaga0214@gmail.com>
2025-06-17 12:55:33 +0900

    Userクラスのインスタンスメソッドに対してテストを書いた

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -3,7 +3,13 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :users
+  test '#name_or_email' do
+    user = users(:default_user)
+
+    assert_equal 'Foo', user.name_or_email
+
+    user.name = ''
+    assert_equal 'foo@example.com', user.name_or_email
+  end
 end
